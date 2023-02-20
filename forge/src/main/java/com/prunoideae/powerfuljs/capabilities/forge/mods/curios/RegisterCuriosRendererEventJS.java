a4e8fadaefa53882d26c9c12b7ff7e62a5f8ba26
Li Junyu <jlijh@connect.ust.hk>
2023-02-20 18:55:05 +0800

    Fixed improper inheritance

--- forge/src/main/java/com/prunoideae/powerfuljs/capabilities/forge/mods/curios/RegisterCuriosRendererEventJS.java
+++ forge/src/main/java/com/prunoideae/powerfuljs/capabilities/forge/mods/curios/RegisterCuriosRendererEventJS.java
@@ -2,12 +2,13 @@ package com.prunoideae.powerfuljs.capabilities.forge.mods.curios;
 
 import com.prunoideae.powerfuljs.PowerfulJS;
 import dev.latvian.mods.kubejs.client.ClientEventJS;
+import dev.latvian.mods.kubejs.event.EventJS;
 import net.minecraft.world.item.Item;
 import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 
 import java.util.function.Consumer;
 
-public class RegisterCuriosRendererEventJS extends ClientEventJS {
+public class RegisterCuriosRendererEventJS extends EventJS {
     public void register(Item item, Consumer<RendererCurios.RenderContext> renderer) {
         PowerfulJS.PROXY.runOnClient(() -> CuriosRendererRegistry.register(item, () -> new RendererCurios(renderer)));
     }
